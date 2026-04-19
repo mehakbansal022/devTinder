86bbf7659950dc935e3d01a27685578d2a1fa979
mehakbansal022 <67435653+mehakbansal022@users.noreply.github.com>
2026-04-19 21:33:07 +0530

    Exploring get, post , delete API calls

--- src/app.js
+++ src/app.js
@@ -1,14 +1,20 @@
 const express = require("express");
 const app =  express();
-app.use("/",(req,res) => {
-    res.send("Namaste from the dashboard!");
+
+app.get("/user",(req,res) => {
+    res.send({FirstName : "Mehak", LastName : "Bansal"});
 });
+app.post("/user",(req,res)=> {
+    res.send("Data Successfully saved to the Database!");
+});
+app.delete("/user",(req,res) =>{
+    res.send("Deleted Successfully!!");
+})
 app.use("/test",(req,res) => {
     res.send("Hello from the server");
 });
-app.use("/hello",(req,res) => {
-    res.send("Hello Hello hello ");
-});
+
+
 
 app.listen(3000 , ()=> {
     console.log("Server is successfully listening on port 3000");
